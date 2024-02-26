e94b98210fd716bff14e8dae667c1bd2f7103bc7
Liliana Lozano <LRS20110117@purisima.tecnm.mx>
2024-02-25 22:26:54 -0600

    Corrección lógica de cálculo

--- lib/SeguimientoSaldo/EstadoFinanciero.dart
+++ lib/SeguimientoSaldo/EstadoFinanciero.dart
@@ -2,25 +2,26 @@ import '../RegistroTransacciones/RegistroFinanzas.dart';
 
 class EstadoFinanciero{
   double saldoActual = 0.0;
-  double gastosTotales = 0.0;
-  double  ingresosTotales = 0.0;
-  
+
+
   TransactionList transactionList;
   EstadoFinanciero(this.transactionList);
 
   void mostrarResumenFinanciero(){
-    for(var transaccion in transactionList.transactions){
-      if(transaccion.type == 'Ingreso'){
-        ingresosTotales += transaccion.amount;
+    double gastosTotales = 0.0;
+    double  ingresosTotales = 0.0;
+      for(var transaccion in transactionList.transactions){
+        if(transaccion.type == 'Ingreso'){
+          ingresosTotales += transaccion.amount;
+        }
+        else{
+          gastosTotales+= transaccion.amount.abs();
+        }
       }
-      else{
-        gastosTotales+= transaccion.amount.abs();
-      }
-    }
-    double saldoActual = ingresosTotales-gastosTotales;
-    print('========RESUMEN DEL ESTADO FINANCIERO========');
-    print('Ingresos: \$${ingresosTotales.toStringAsFixed(2)}');
-    print('Gastos: \$${gastosTotales.toStringAsFixed(2)}');
-    print('Saldo: \$${saldoActual.toStringAsFixed(2)}');
+      saldoActual = ingresosTotales-gastosTotales;
+      print('========RESUMEN DEL ESTADO FINANCIERO========');
+      print('Ingresos: \$${ingresosTotales.toStringAsFixed(2)}');
+      print('Gastos: \$${gastosTotales.toStringAsFixed(2)}');
+      print('Saldo: \$${saldoActual.toStringAsFixed(2)}');
   }
 }
